de244362ce4a3b9cfe2b1ac7ba2a269a136b385a
zlattw <samusevazlata6@gmail.com>
2024-10-27 01:20:59 +0300

    first project

--- hw1.py
+++ hw1.py
@@ -1,1 +1,5 @@
-print("Hello World")
+name = input("Введи своє ім'я - ")
+surname = input("Введи своє прізвище - ")
+age = input("Введи свій вік - ")
+
+print("Привіт, мене звати", surname, name, ". Мені", age, "років!")
