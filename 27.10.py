cea02660371f5bf557677be80d7b1196491aa783
zlattw <samusevazlata6@gmail.com>
2024-11-02 00:09:25 +0200

    first project, 02.11

--- 27.10.py
+++ 27.10.py
@@ -7,26 +7,44 @@ class NameSurname:
 
 class Student:
     student_amount = 0
-    birthday = 0
     def HappyBirthday(self):
         self.age +=1
-        print(f"Happy Birthday to {self.ns.name}. Now you {self.age}!")
+        print(f"Happy Birthday to {self.name}. Now you {self.age}!")
 
-    def __init__ (self,name, surname,age, height):
-        self.ns = NameSurname(name, surname)
+    def __init__ (self, name, surname,age, height, school = "itstep"):
+        self.name = name
+        self.surname = surname
         self.age = age
         self.height = height
+        self.school = school
         Student.student_amount += 1
 
 
     def printStudent(self):
-        print(f"Name: {self.ns.name}")
-        print(f"Surname: {self.ns.surname}")
+        print(f"Name: {self.name}")
+        print(f"Surname: {self.surname}")
         print(f"Age: {self.age}")
         print(f"Height: {self.height}")
 
+    def School(self):
+        return bool(self.school)
+
+    def grade_point_average(self, grade):
+        if grade == 12:
+            print("Відмінно")
+        elif grade >= 10:
+            print("Добре")
+        elif grade >= 7:
+            print("Задовільно")
+        elif grade >= 4:
+            print("Погано")
+        elif grade >= 1:
+            print("Дуже погано")
+
+
+
 print(f"Before creating Student object - {Student.student_amount}")
-Zlata = Student("Zlata", "Samuseva", 12, 175)
+Zlata = Student("Zlata", "Samuseva", 15, 175, )
 print(f"Before creating Student object - {Student.student_amount}")
 first_student = Student(input("Enter name - "), input("Enter surname - "), input("Enter age - "), input("Enter height - "))
 
@@ -37,3 +55,6 @@ Zlata.printStudent()
 print(f"Student amount - {Student.student_amount}")
 print(f"After creating Student object - {Student.student_amount}")
 
+print(Zlata.School())
+average_grade = int(input("Введіть середній бал студента - "))
+print(Zlata.grade_point_average(average_grade))
